9c54d0c171c5e91a9a7df1e2abd42b293e929d9d
xheronimo <javi_direc@hotmail.com>
2025-12-26 10:52:49 +0100

    Delete src directory

--- src/digital.h
+++
@@ -1,8 +0,0 @@
-// src/digital.h - Archivo de definiciones de entradas digitales
-#ifndef DIGITAL_H
-#define DIGITAL_H
-
-void inicializarEntradasDigitales();
-void procesarEntradasDigitales();
-
-#endif
